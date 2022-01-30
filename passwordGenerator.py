de5e42822b919ee8cb5af2d898eae503f3cd0c67
Oğuz Esirik <79198109+aidaa64@users.noreply.github.com>
2022-01-30 17:29:18 +0300

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import random
+import string
+
+print("Hello!, Welcome to password generator.")
+
+def generate_pass():
+	while True:		
+		try:
+			length = int(input("Enter the password length: "))
+			break		
+		except ValueError:
+			print("Enter a number!")
+
+
+	lower_case = string.ascii_lowercase
+	upper_case = string.ascii_uppercase
+	numbers = string.digits
+	symbols = "!@#$%^&_/*"
+
+	character = list(lower_case + upper_case + numbers + symbols)
+
+
+	random.shuffle(character)
+
+	print(character)
+
+	temp = random.sample(character,length)
+
+	password = "".join(temp)
+
+	
+
+	print("Your "+str(length)+" digit password is: " + password)
+	
+generate_pass()
